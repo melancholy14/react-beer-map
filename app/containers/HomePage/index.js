4395acca99e5c0de4bd063e0a84471f05ebcfd70
Misol Goh <msg1y15@soton.ac.uk>
2018-04-02 18:27:49 +0100

    Feat: Install react-vis
    Install react-vis, a package for helping visualise data
    Try Getting started
     https://uber.github.io/react-vis/documentation/getting-started/your-first-chart

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,15 +10,44 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import {
+  XYPlot,
+  LineSeries,
+  VerticalGridLines,
+  HorizontalGridLines,
+  XAxis,
+  YAxis,
+} from 'react-vis';
+
+import '../../../node_modules/react-vis/dist/style.css';
+
+// import { FormattedMessage } from 'react-intl';
+// import messages from './messages';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const data = [
+      { x: 0, y: 8 },
+      { x: 1, y: 5 },
+      { x: 2, y: 4 },
+      { x: 3, y: 9 },
+      { x: 4, y: 1 },
+      { x: 5, y: 7 },
+      { x: 6, y: 6 },
+      { x: 7, y: 3 },
+      { x: 8, y: 2 },
+      { x: 9, y: 0 }];
+
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div className="App">
+        <XYPlot height={300} width={300}>
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis />
+          <YAxis />
+          <LineSeries data={data} />
+        </XYPlot>
+      </div>
     );
   }
 }
