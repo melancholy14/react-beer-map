0caedfe32016c132c8812af7bfaebb5dcbff0b42
Misol <misol.goh@gmail.com>
2018-04-03 23:45:54 +0100

    Feat: Try Tutorial Try General Principles / API Reference in React-vis documentation

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,11 +12,14 @@
 import React from 'react';
 import {
   XYPlot,
+  VerticalBarSeries,
   LineSeries,
+  MarkSeries,
   VerticalGridLines,
   HorizontalGridLines,
   XAxis,
   YAxis,
+  Hint,
 } from 'react-vis';
 
 import '../../../node_modules/react-vis/dist/style.css';
@@ -25,8 +28,18 @@ import '../../../node_modules/react-vis/dist/style.css';
 // import messages from './messages';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor() {
+    super();
+
+    this.state = {
+      value: {},
+    };
+  }
+
+  onValueClick = (value) => console.log(value);
+
   render() {
-    const data = [
+    const series1 = [
       { x: 0, y: 8 },
       { x: 1, y: 5 },
       { x: 2, y: 4 },
@@ -38,6 +51,18 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
       { x: 8, y: 2 },
       { x: 9, y: 0 }];
 
+    const series2 = [
+      { x: 0, y: 16 },
+      { x: 1, y: 2 },
+      { x: 2, y: 5 },
+      { x: 3, y: 2 },
+      { x: 4, y: 7 },
+      { x: 5, y: 0 },
+      { x: 6, y: 12 },
+      { x: 7, y: 25 },
+      { x: 8, y: 9 },
+      { x: 9, y: 3 }];
+
     return (
       <div className="App">
         <XYPlot height={300} width={300}>
@@ -45,7 +70,42 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
           <HorizontalGridLines />
           <XAxis />
           <YAxis />
-          <LineSeries data={data} />
+          <VerticalBarSeries
+            data={series1}
+          />
+          <VerticalBarSeries
+            data={series2}
+          />
+        </XYPlot>
+        <XYPlot
+          height={300}
+          width={300}
+          colorDomain={[0, 1]}
+        >
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis />
+          <YAxis />
+          <LineSeries
+            data={series1}
+            color={0}
+          />
+          <LineSeries
+            data={series2}
+            color={1}
+          />
+          <MarkSeries
+            data={series1}
+            color="#cdcdcd"
+            onValueClick={(value) => this.setState({ value })}
+          >
+            <Hint value={this.state.value} />
+          </MarkSeries>
+          <MarkSeries
+            data={series2}
+            color="#ab0000"
+            onValueClick={this.onValueClick}
+          />
         </XYPlot>
       </div>
     );
